5db47c7463c5f11738fc7025d03decdf45ce64d6
dipanshu_tihal_ <dtihal.dipanshu@gmail.com>
2024-12-14 19:30:33 -0500

    added db connection

--- public/classes/db1.php
+++ public/classes/db1.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Sanchalana2k20";
+$servername = "campus-connect-db.chm0q2ogqbv7.us-east-2.rds.amazonaws.com";  
+$username = "admin"; 
+$password = "campusconnectdb"; 
+$dbname = "campusconnectdb";  
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
